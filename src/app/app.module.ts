6f932f8ae4672a48a577f634777b55cb68c46ba4
Rubin Bajracharya <rubin@fusemachines.com>
2018-05-24 22:47:32 +0545

    Add Navigation Schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,32 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import { MatButtonModule } from '@angular/material'
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule
+} from '@angular/material'
+import { MyNavComponent } from './my-nav/my-nav.component'
+import { LayoutModule } from '@angular/cdk/layout'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+  declarations: [AppComponent, MyNavComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
