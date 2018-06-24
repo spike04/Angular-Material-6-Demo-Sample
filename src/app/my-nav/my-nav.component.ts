ad8759ffb0af1322ff85a4c1cbdfc7cf15536100
Rubin Bajracharya <rubin@FuseMachines-Mac.local>
2018-06-24 15:26:13 +0545

    Demo of Second Side Navigation Drawer and Removing Toolbar from Drawer

--- src/app/my-nav/my-nav.component.ts
+++ src/app/my-nav/my-nav.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component } from '@angular/core'
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState
+} from '@angular/cdk/layout'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'my-nav',
@@ -9,12 +13,11 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./my-nav.component.css']
 })
 export class MyNavComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches))
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
-    
+  secondaryOpened = true
+
   constructor(private breakpointObserver: BreakpointObserver) {}
-  
-  }
+}
