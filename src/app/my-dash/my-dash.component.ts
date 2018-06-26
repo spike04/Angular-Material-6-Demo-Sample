851cba8ae970b1ff6f854d20f987443413fc4a69
Rubin Bajracharya <rubin@FuseMachines-Mac.local>
2018-06-26 22:50:46 +0545

    Added a breakpoint for making the layout behave as per different screens

--- src/app/my-dash/my-dash.component.ts
+++ src/app/my-dash/my-dash.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState
+} from '@angular/cdk/layout'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'my-dash',
@@ -6,10 +13,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-dash.component.css']
 })
 export class MyDashComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches))
+
   cards = [
     { title: 'Card 1', cols: 2, rows: 1 },
     { title: 'Card 2', cols: 1, rows: 1 },
     { title: 'Card 3', cols: 1, rows: 2 },
     { title: 'Card 4', cols: 1, rows: 1 }
-  ];
+  ]
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
